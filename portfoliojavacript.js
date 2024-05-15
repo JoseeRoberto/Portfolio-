5a3478d5a338e2078742babb3dff9dcf65dc3f32
José Roberto <joser486430@gmail.com>
2024-05-15 14:09:38 -0300

    Arquivos que compõe o Portfolio
    GIFS, HTML, CSS e JavaScript

---
+++ portfoliojavacript.js
@@ -0,0 +1,9 @@
+function toggleSectionContent(element){
+    var content = element.nextElementSibling;
+    if(content.style.display === "none"){
+        content.style.display = "block";
+    }
+    else{
+     content.style.display = "none";
+    }
+}
